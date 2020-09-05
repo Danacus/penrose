12ffeff73c59529b551b6d4c7b29f1cdf8ac4bbc
Daan Vanoverloop <vanoverloopdaan@gmail.com>
2020-09-05 11:01:53 +0200

    Scratchpad positioning using Anchors
    - Allows you to position Scratchpads by specifying sides/corners and offsets

--- src/contrib/extensions/scratchpad.rs
+++ src/contrib/extensions/scratchpad.rs
@@ -10,6 +10,65 @@ use crate::core::{
 use std::{cell::RefCell, rc::Rc};
 
 /**
+ * Position of an Anchor
+ * 'Top' and 'Bottom' should only be used for the vertical position
+ * 'Left' and 'Right' should only be used for the horizontal position
+ * 'Center' can be used for either
+ */
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub enum AnchorPosition {
+    /// Top of the screen
+    Top,
+    /// Center of the screen
+    Center,
+    /// Bottom of the screen
+    Bottom,
+    /// Left of the screen
+    Left,
+    /// Right of the screen
+    Right,
+}
+
+pub use AnchorPosition::*;
+
+/// Where a Scratchpad should be placed on the screen
+#[derive(Debug, Clone, Copy)]
+pub struct Anchor {
+    horizontal: AnchorPosition,
+    vertical: AnchorPosition,
+    offset: (u32, u32),
+}
+
+impl Anchor {
+    /**
+     * Create a new Anchor for a Scratchpad. 'horizontal' and 'vertical' determine the Anchor
+     * point. 'offset' determines the offset from the Anchor point (in pixels).
+     */
+    pub fn new(horizontal: AnchorPosition, vertical: AnchorPosition, offset: (u32, u32)) -> Self {
+        if horizontal == Top || horizontal == Bottom {
+            panic!("Scratchpad: invalid horizontal anchor {:?}", horizontal);
+        }
+
+        if vertical == Left || vertical == Right {
+            panic!("Scratchpad: invalid vertical anchor {:?}", vertical);
+        }
+
+        Self {
+            horizontal,
+            vertical,
+            offset,
+        }
+    }
+
+    /**
+     * Create a new Achor in the center of the screen with no offset.
+     */
+    pub fn default() -> Self {
+        Self::new(Center, Center, (0, 0))
+    }
+}
+
+/**
  * A Scratchpad spawns and manages a single Client which can then be shown above the current layout
  * using the 'toggle' method when bound to a key combination in your main.rs. The
  * Scratchpad.register method must be called before creating your WindowManager struct in order to
@@ -25,13 +84,14 @@ pub struct Scratchpad {
     prog: &'static str,
     w: f32,
     h: f32,
+    anchor: Anchor,
 }
 
 impl Scratchpad {
     /// Create a new Scratchpad for holding 'prog'. 'w' and 'h' are the percentage width and height
     /// of the active screen that you want the client to take up when visible.
     /// NOTE: this function will panic if 'w' or 'h' are not within the range 0.0 - 1.0
-    pub fn new(prog: &'static str, w: f32, h: f32) -> Scratchpad {
+    pub fn new(prog: &'static str, w: f32, h: f32, anchor: Anchor) -> Scratchpad {
         if w < 0.0 || w > 1.0 || h < 0.0 || h > 1.0 {
             panic!("Scratchpad: w & h must be between 0.0 and 1.0");
         }
@@ -43,6 +103,7 @@ impl Scratchpad {
             prog,
             w,
             h,
+            anchor,
         }
     }
 
@@ -54,6 +115,7 @@ impl Scratchpad {
             prog: self.prog,
             w: self.w,
             h: self.h,
+            anchor: self.anchor,
         })
     }
 
@@ -89,6 +151,28 @@ impl Scratchpad {
             self.visible.replace(true);
         }
     }
+
+    fn get_position(&self, screen: Region) -> Region {
+        let (sx, sy, sw, sh) = screen.values();
+        let w = (sw as f32 * self.w) as u32;
+        let h = (sh as f32 * self.h) as u32;
+
+        let x = match self.anchor.horizontal {
+            Left => sx,
+            Right => sx + sw - w,
+            Center => sx + (sw - w) / 2,
+            _ => unreachable!(),
+        } + self.anchor.offset.0;
+
+        let y = match self.anchor.vertical {
+            Top => sy,
+            Bottom => sy + sh - h,
+            Center => sy + (sh - h) / 2,
+            _ => unreachable!(),
+        } + self.anchor.offset.1;
+
+        Region::new(x, y, w, h)
+    }
 }
 
 impl Hook for Scratchpad {
@@ -118,12 +202,7 @@ impl Hook for Scratchpad {
             None => return, // no active scratchpad client
             Some(id) => {
                 if let Some(region) = wm.screen_size(screen_index) {
-                    let (sx, sy, sw, sh) = region.values();
-                    let w = (sw as f32 * self.w) as u32;
-                    let h = (sh as f32 * self.h) as u32;
-                    let x = sx + (sw - w) / 2;
-                    let y = sy + (sh - h) / 2;
-                    wm.position_client(id, Region::new(x, y, w, h));
+                    wm.position_client(id, self.get_position(region));
                 }
             }
         }
